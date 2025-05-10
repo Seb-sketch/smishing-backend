d30771e83f805522cb6c5e724844875e6db9d226
Seb-sketch <sebastianedge50@gmail.com>
2025-05-10 16:46:18 +1000

    Update news.route.js

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -1,8 +1,1 @@
-import express from 'express';
-const router = express.Router();
-import { searchNews } from '../controllers/news.controller.js';
 
-
-router.get('/search', searchNews);
-
-export default router;
